10446e200215404fd4a73cbb1ee512eed140c0cb
Carlos Felipe <carlosfelipe921sm@gmail.com>
2026-01-20 22:58:25 -0300

    algoritimos de busca

---
+++ cs-core/algorithms/binary_search.js
@@ -0,0 +1,22 @@
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] === target) return arr[mid]; // Retorna o elemento encontrado
+        
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return "Não Encontrado"; // Retorna mensagem se o elemento não for encontrado
+}
+
+const fruits = ["maçã", "banana", "cereja", "figo", "laranja", "manga", "pera"];
+
+console.log(binarySearch(fruits, "uva")); 
+console.log(binarySearch(fruits, "figo")); 
