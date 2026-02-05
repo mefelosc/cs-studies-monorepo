7ba62a429861c7262cec8eaa4e6f4436f501a6a2
Carlos Felipe <carlosfelipe921sm@gmail.com>
2026-02-05 11:26:28 -0300

    added node snippets from fefs

---
+++ courses/frontendmasters/nodejs/src/command.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+  .command('curl <url> ', 'Fetch content from a URL', () => {},
+    console.info(argv)
+)  
+    .demandCommand(1)
+    .parse()
