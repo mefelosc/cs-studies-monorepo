9f2e52d27611533ea22d556ae06ef721f5e4312a
Carlos Felipe <carlosfelipe921sm@gmail.com>
2026-02-05 12:41:14 -0300

    yargs examples

--- courses/frontendmasters/nodejs/src/command.js
+++ courses/frontendmasters/nodejs/src/command.js
@@ -3,8 +3,8 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 yargs(hideBin(process.argv))
-  .command('curl <url> ', 'Fetch content from a URL', () => {},
+  .command('curl <url> ', 'Fetch content from a URL', () => {}, (argv) => {
     console.info(argv)
-)  
+  })  
     .demandCommand(1)
     .parse()
